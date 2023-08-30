91837e0e666cf4aef5a3ce61d4150e120d1e2a18
“c.b.olha” <c.b.olha@viseven.com>
2023-08-30 11:45:39 -0300

    added footer styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,44 @@
+import "../styles/Footer.scss";
+import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
+
 const Footer = () => {
-  return <p>Footer</p>;
+  return (
+    <footer>
+      <p className="author">Designed and developed with 💪 by Candela Olha</p>
+      <ul className="social-media-icons">
+        <li className="social-media-item">
+          <a
+            href="https://github.com/CandelaOlha"
+            target="_blank"
+            className="social-media-link"
+            rel="noreferrer"
+          >
+            <FaGithub />
+          </a>
+        </li>
+        <li className="social-media-item">
+          <a
+            href="https://www.linkedin.com/in/candela-olha/"
+            target="_blank"
+            className="social-media-link"
+            rel="noreferrer"
+          >
+            <FaLinkedin />
+          </a>
+        </li>
+        <li className="social-media-item">
+          <a
+            href="mailto:olhacandela@gmail.com"
+            target="_blank"
+            className="social-media-link"
+            rel="noreferrer"
+          >
+            <FaEnvelope />
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
 };
 
 export default Footer;
