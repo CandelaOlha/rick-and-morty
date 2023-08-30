e7ba5a14456b5f45bfd6a8cd432f3105efe77963
“c.b.olha” <c.b.olha@viseven.com>
2023-08-30 10:53:01 -0300

    added layout and page not found

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainLayout from "./components/layouts/MainLayout";
 import Characters from "./components/Characters";
 import Locations from "./components/Locations";
 import Episodes from "./components/Episodes";
+import PageNotFound from "./components/PageNotFound";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Characters />} path="/"></Route>
-        <Route element={<Locations />} path="/locations"></Route>
-        <Route element={<Episodes />} path="/episodes"></Route>
+        <Route element={<MainLayout />} path="/">
+          <Route element={<Characters />} path="/"></Route>
+          <Route element={<Locations />} path="/locations"></Route>
+          <Route element={<Episodes />} path="/episodes"></Route>
+        </Route>
+        <Route element={<PageNotFound />} path="*"></Route>
       </Routes>
     </BrowserRouter>
   );
