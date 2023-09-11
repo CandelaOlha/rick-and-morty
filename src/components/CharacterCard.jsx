466e365eec3c2c96779eb8fbaa14453f4d9a97a7
“c.b.olha” <c.b.olha@viseven.com>
2023-09-11 14:50:24 -0300

    added more styles to characters container

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -2,6 +2,16 @@ import "../styles/CharacterCard.scss";
 import PropTypes from "prop-types";
 
 const CharacterCard = ({ character }) => {
+  const addStatusColor = () => {
+    if (character.status === "Alive") {
+      return "alive";
+    } else if (character.status === "Dead") {
+      return "dead";
+    } else {
+      return "unknown";
+    }
+  };
+
   return (
     <article className="character-card">
       <img
@@ -12,17 +22,19 @@ const CharacterCard = ({ character }) => {
       <div className="character-info">
         <h3 className="character-name">{character.name}</h3>
         <div className="character-status">
-          <div className="character-status-color"></div>
+          <div className={`character-status-color ${addStatusColor()}`}></div>
           <p className="character-status-text">
             {character.status} - {character.species}
           </p>
         </div>
-        <p className="character-origin-text">
-          First seen in: {character.origin.name}
-        </p>
-        <p className="character-location-text">
-          Last known location: {character.location.name}
-        </p>
+        <div className="character-container">
+          <p className="character-title">First seen in:</p>
+          <p className="character-text">{character.origin.name}</p>
+        </div>
+        <div className="character-container">
+          <p className="character-title">Last known location:</p>
+          <p className="character-text">{character.location.name}</p>
+        </div>
       </div>
     </article>
   );
