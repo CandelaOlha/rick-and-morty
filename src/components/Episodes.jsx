02386b66895e0669aff313dbf3e3422c8d67ef9b
“c.b.olha” <c.b.olha@viseven.com>
2023-09-13 15:02:55 -0300

    added episodes cards

--- src/components/Episodes.jsx
+++ src/components/Episodes.jsx
@@ -1,5 +1,25 @@
+import "../styles/Episodes.scss";
+import { useState, useEffect } from "react";
+import EpisodeCard from "./EpisodeCard";
+
 const Episodes = () => {
-  return <h1>Episodes</h1>;
+  const [episodes, setEpisodes] = useState([]);
+
+  useEffect(() => {
+    fetch("https://rickandmortyapi.com/api/episode")
+      .then((res) => res.json())
+      .then((data) => setEpisodes(data.results));
+  }, [episodes]);
+
+  return (
+    <div className="episodes-main-view-container">
+      <main className="episodes-main-view">
+        {episodes.map((episode) => (
+          <EpisodeCard key={episode.id} episode={episode} />
+        ))}
+      </main>
+    </div>
+  );
 };
 
 export default Episodes;
